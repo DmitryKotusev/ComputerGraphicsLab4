0d860546792eb28b9a39952ec367e4f735d08bd9
DmitryKotusev <dssa-1966@yandex.ru>
2019-04-17 00:42:18 +0300

    Started making camera.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public bool mouseWheelUp;
+    public bool mouseWheelDown;
+
+    private void Update()
+    {
+        mouseWheelUp = Input.GetAxis("Mouse ScrollWheel") > 0;
+        mouseWheelDown = Input.GetAxis("Mouse ScrollWheel") < 0;
+    }
+}
