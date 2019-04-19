1312fed31c588cc46754f568ed5c5b187623a827
DmitryKotusev <dssa-1966@yandex.ru>
2019-04-19 13:01:19 +0300

    Made skeleton of camera controller.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,16 +6,32 @@ public class CameraController : MonoBehaviour
 {
     InputManager inputManager;
 
-    [SerializeField] float minZoom = 3f;
-    [SerializeField] float maxZoom = 12f;
+    [SerializeField] float minDistanceToGrid = 3f;
+    [SerializeField] float maxDistanceToGrid = 12f;
+    [SerializeField] Vector2 boundaries = new Vector2(12, 12);
     [SerializeField] Vector3 startCameraPosition = new Vector3(0, 12, 0);
     public float zoomStep = 0.5f;
 
     void Start()
     {
+        inputManager = GameObject.FindGameObjectWithTag("InputManager").GetComponent<InputManager>();
     }
 
     void Update()
     {
+        if (inputManager.middleMouseButtonPress)
+        {
+            HandleCameraMotion();
+        }
+    }
+
+    void HandleCameraMotion()
+    {
+
+    }
+
+    void HandleCameraZoom()
+    {
+
     }
 }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    public float horizontalMouseInput;
+    public float verticalMouseInput;
     public bool mouseWheelUp;
     public bool mouseWheelDown;
     public bool leftMouseButtonClick;
     public bool rightMouseButtonClick;
+    public bool middleMouseButtonPress;
 
     private void Update()
     {
+        horizontalMouseInput = Input.GetAxis("Mouse X");
+        verticalMouseInput = Input.GetAxis("Mouse Y");
         mouseWheelUp = Input.GetAxis("Mouse ScrollWheel") > 0;
         mouseWheelDown = Input.GetAxis("Mouse ScrollWheel") < 0;
         leftMouseButtonClick = Input.GetMouseButtonDown(0);
         rightMouseButtonClick = Input.GetMouseButtonDown(1);
+        middleMouseButtonPress = Input.GetMouseButton(2);
     }
 }
