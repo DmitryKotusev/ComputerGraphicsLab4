c5b249f57b5442a34f9644e9e4f34a44fde519ad
DmitryKotusev <dssa-1966@yandex.ru>
2019-04-19 13:57:00 +0300

    Camera control needs rework.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CameraController : MonoBehaviour
 {
     InputManager inputManager;
 
+    public float zoomSpeed = 0.2f;
+    public float motionSpeed = 10f;
+    [SerializeField] Texture2D grabHandTexture;
     [SerializeField] float minDistanceToGrid = 3f;
     [SerializeField] float maxDistanceToGrid = 12f;
     [SerializeField] Vector2 boundaries = new Vector2(12, 12);
@@ -19,19 +23,34 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        if (inputManager.middleMouseButtonPress)
+        if (inputManager.middleMouseButtonPress && !EventSystem.current.IsPointerOverGameObject())
         {
+            Cursor.SetCursor(grabHandTexture, Vector2.zero, CursorMode.Auto);
             HandleCameraMotion();
         }
+        HandleCameraZoom();
     }
 
     void HandleCameraMotion()
     {
-
+        Vector3 oldMousePosition = transform.localToWorldMatrix * Input.mousePosition;
+        Vector3 motionVector = (new Vector3(-inputManager.horizontalMouseInput, -inputManager.verticalMouseInput, 0f)).normalized;
+        transform.Translate(motionVector * motionSpeed * Time.deltaTime);
+        Input.Mou
     }
 
     void HandleCameraZoom()
     {
-
+        float newY = transform.position.y;
+        if (inputManager.mouseWheelDown)
+        {
+            newY += zoomSpeed * Time.deltaTime;
+        }
+        if (inputManager.mouseWheelUp)
+        {
+            newY -= zoomSpeed * Time.deltaTime;
+        }
+        newY = Mathf.Clamp(newY, minDistanceToGrid, maxDistanceToGrid);
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
     }
 }
